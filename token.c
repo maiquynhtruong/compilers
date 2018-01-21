3d20144d424cfa37da8e5a426576534916cd0836
Mai Truong <maitruong2801@gmail.com>
2018-01-20 21:23:09 -0500

    add gitignore and add reserved words to token.c

--- token.c
+++ token.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "token.h"
-
+// map structure: https://github.com/soywod/c-map/blob/master/map.c
 typedef struct {
     char keyword[MAX_STRING_LENGTH];
     TokenType type;
 } Keyword;
+
 Keyword keywords[MAX_KEYWORD];
 
 void insert_keyword(char *keyword, TokenType type) {
@@ -18,10 +19,39 @@ void insert_keyword(char *keyword, TokenType type) {
 
 void insert_all_keywords() {
     insert_keyword("PROGRAM", K_PROGRAM);
+    insert_keyword("IS", K_IS);
+    insert_keyword("global", K_GLOBAL);
+    insert_keyword("in", K_IN);
+    insert_keyword("out", K_OUT);
+    insert_keyword("inout", K_INOUT);
+    insert_keyword("int", K_INT);
+    insert_keyword("float", K_FLOAT);
+    insert_keyword("bool", K_BOOL);
+    insert_keyword("char", K_CHAR);
+    insert_keyword("procedure", K_PROCEDURE);
+    insert_keyword("return", K_RETURN);
+    insert_keyword("not", K_NOT);
+    insert_keyword("begin", K_BEGIN);
+    insert_keyword("end", K_END);
+    insert_keyword("if", K_IF);
+    insert_keyword("else", K_ELSE);
+    insert_keyword("then", K_THEN);
+    insert_keyword("true", K_TRUE);
+    insert_keyword("false", K_FALSE);
+    insert_keyword("for", K_FOR);
+    insert_keyword("while", K_WHILE);
+}
+
+int check_reserved_word(char *str) {
+    int i, n = sizeof(keywords)/sizeof(keywords[0]);
+    for (i = 0; i < n; i++) {
+        if (strcmp(keywords[i].keyword, str)) 
+            return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     insert_all_keywords();
     printf("%s%d\n", keywords[0].keyword, keywords[0].type);
-
 }
--- token.h
+++ token.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #define MAX_STRING_LENGTH 50
 #define MAX_KEYWORD 30
+// enum type: https://www.geeksforgeeks.org/enumeration-enum-c/
 typedef enum {
 T_IDENTIFIER,
     T_END_OF_FILE, T_ASSIGNMENT, T_RELATION, T_NUMBER, 
@@ -11,7 +12,7 @@ T_IDENTIFIER,
     K_IN, K_OUT, K_INOUT,
     K_INT, K_FLOAT, K_BOOL, K_CHAR,
     K_PROCEDURE, K_RETURN, K_NOT, 
-    K_BEGIN, _END, 
+    K_BEGIN, K_END, 
     K_IF, K_ELSE, K_THEN, 
     K_TRUE, K_FALSE, 
     K_FOR, K_WHILE, 
