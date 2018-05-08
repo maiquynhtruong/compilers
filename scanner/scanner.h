0bccace4d1e00ddaa0671713e9eebecbfaa12611
Mai Truong <maitruong2801@gmail.com>
2018-05-07 21:51:03 -0400

    delete scanner.h

--- scanner/scanner.h
+++
@@ -1,9 +0,0 @@
-#include "token.h"
-
-TokenType next_token(Token *token);
-void init_scanner(char *fileName);
-TokenType check_reserved_word(char *tokenStr);
-void print_token(Token *token);
-void skip_star_comment();
-void reset_token(Token *token);
-
