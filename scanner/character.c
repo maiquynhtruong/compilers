103bad23fd2fba754fcae228a4df914235f16379
Mai Truong <maitruong2801@gmail.com>
2018-05-14 22:03:32 -0400

    implement character class

---
+++ scanner/character.c
@@ -0,0 +1,8 @@
+#include "character.h"
+
+// an array indexed by the input character that maps an
+// ASCII character into a character class
+
+CharClass charClasses[256] = {
+    
+};
--- scanner/character.h
+++ scanner/character.h
@@ -1,2 +1,29 @@
-// an array indexed by the input character that maps an
-// ASCII character into a character class
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+typedef enum {
+    C_SPACE,
+    C_LETTER,
+    C_DIGIT,
+    C_SLASH,
+    C_STAR,
+    C_SINGLE_QUOTE,
+    C_DOUBLE_QUOTE,
+    C_COLON,
+    C_SEMI_COLON,
+    C_COMMA,
+    C_PLUS,
+    C_MINUS,
+    C_LESS_THAN,
+    C_EQUAL,
+    C_GREATER_THAN,
+    C_EXCLAIMATION,
+    C_LEFT_PAREN,
+    C_RIGHT_PAREN,
+    C_LEFT_BRACKET,
+    C_RIGHT_BRACKET,
+    C_PERIOD,
+    C_UNKNOWN
+} CharClass;
+
+#endif
