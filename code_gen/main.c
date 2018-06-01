f6d845cfd9d88de7a7d58e4ed256a82454177d15
Mai Truong <maitruong2801@gmail.com>
2018-05-31 23:11:39 -0400

    add main.c file

---
+++ code_gen/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/Transforms/Scalar.h>
+
+#include "ast.h"
+#include "parser.h"
+#include "codegen.h"
+
+int main(int argc, char **argv) {
+    LLVMModuleRef module = LLVMModuleCreateWithName("compiler");
+    LLVMBuilderRef builder = LLVMCreateBuilder();
+    LLVMExecutionEngineRef engine;
+
+    LLVMInitializeNativeTarget();
+    LLVMLinkInJIT();
+
+    // Create execution engine.
+    char *msg;
+    if(LLVMCreateExecutionEngineForModule(&engine, module, &msg) == 1) {
+        fprintf(stderr, "%s\n", msg);
+        LLVMDisposeMessage(msg);
+        return 1;
+    }
+
+    // Setup optimizations.
+    LLVMPassManagerRef pass_manager =  LLVMCreateFunctionPassManagerForModule(module);
+    LLVMAddTargetData(LLVMGetExecutionEngineTargetData(engine), pass_manager);
+    LLVMAddPromoteMemoryToRegisterPass(pass_manager);
+    LLVMAddInstructionCombiningPass(pass_manager);
+    LLVMAddReassociatePass(pass_manager);
+    LLVMAddGVNPass(pass_manager);
+    LLVMAddCFGSimplificationPass(pass_manager);
+    LLVMInitializeFunctionPassManager(pass_manager);
+}
