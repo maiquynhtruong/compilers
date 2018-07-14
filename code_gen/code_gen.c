ef1e337db12c6a54a7dd11bf1e27e1bf745eb512
Mai Truong <maitruong2801@gmail.com>
2018-07-14 18:05:20 -0400

    fix complie errors

--- code_gen/code_gen.c
+++ code_gen/code_gen.c
@@ -9,30 +9,34 @@
 #include <llvm-c/Analysis.h>
 #include <llvm-c/BitWriter.h>
 
+#include "code_gen.h"
+#include "reader.h"
+#include "parser.h"
+
 LLVMModuleRef module;
 LLVMBuilderRef builder;
 LLVMExecutionEngineRef engine;
 LLVMValueRef llvm_printf;
 
 // https://stackoverflow.com/questions/1061753/how-can-i-implement-a-string-data-type-in-llvm
-LLVMValueRef LLMVCodeGenStringType(LLVMModuleRef module, const char *data, int length) {
-    LLVMValueRef ref = LLVMAddGlobal(module, LLVMArrayType(LLVMInt8Type(), length), "string");
+// LLVMValueRef codegen_string(LLVMModuleRef module, const char *data, int length) {
+//     LLVMValueRef ref = LLVMAddGlobal(module, LLVMArrayType(LLVMInt8Type(), length), "string");
+//
+//   // set as internal linkage and constant
+//   LLVMSetLinkage(ref, LLVMInternalLinkage);
+//   LLVMSetGlobalConstant(ref, 1);
+//
+//   // Initialize with string:
+//   LLVMSetInitializer(ref, LLVMConstString(data, length, 1));
+//
+//   return ref;
+// }
 
-  // set as internal linkage and constant
-  LLVMSetLinkage(ref, LLVMInternalLinkage);
-  LLVMSetGlobalConstant(ref, 1);
-
-  // Initialize with string:
-  LLVMSetInitializer(ref, LLVMConstString(data, length, 1));
-
-  return ref;
-}
-
-LLVMValueRef codegen_declare_proc(char *name, LLVMValueRef *params) {
+LLVMValueRef codegen_declare_proc(char *name, LLVMTypeRef *params) {
     LLVMTypeRef proc_type = LLVMFunctionType(LLVMVoidType(), params, 0, false);
+    LLVMValueRef proc = LLVMAddFunction(module, name, proc_type);
     LLVMBasicBlockRef entry = LLVMAppendBasicBlock(proc, "entry");
     LLVMPositionBuilderAtEnd(builder, entry);
-    LLVMValueRef proc = LLVMAddFunction(module, name, proc_type);
     LLVMBuildRetVoid(builder);
     return proc;
 }
@@ -47,11 +51,11 @@ LLVMValueRef codegen_proc_call(char *name, LLVMValueRef *args, int argc) {
 void codegen_extern_decl() {
     LLVMTypeRef param_types[] = { LLVMPointerType(LLVMInt8Type(), 0) };
 	LLVMTypeRef llvm_printf_type = LLVMFunctionType(LLVMInt32Type(), param_types, 0, true);
-	llvm_printf = LLVMAddFunction(mod, "printf", llvm_printf_type);
+	llvm_printf = LLVMAddFunction(module, "printf", llvm_printf_type);
 }
 
 void codegen_module(char *file_name) {
-    module = LLVMModuleCreateWithName(name);
+    module = LLVMModuleCreateWithName(file_name);
     // builder = LLVMCreateBuilder();
 
     if (parse(file_name) == IO_ERROR) {
@@ -60,7 +64,7 @@ void codegen_module(char *file_name) {
     }
 
     char *error = NULL;
-    LLVMVerifyModule(mod, LLVMAbortProcessAction, &error);
+    LLVMVerifyModule(module, LLVMAbortProcessAction, &error);
     LLVMDisposeMessage(error);
 
     error = NULL;
@@ -68,15 +72,15 @@ void codegen_module(char *file_name) {
     LLVMInitializeNativeTarget();
     LLVMInitializeNativeAsmPrinter();
     LLVMInitializeNativeAsmParser();
-    if (LLVMCreateExecutionEngineForModule(&engine, mod, &error) != 0) {
+    if (LLVMCreateExecutionEngineForModule(&engine, module, &error) != 0) {
         fprintf(stderr, "failed to create execution engine\n");
         abort();
     }
 
     // Write out bitcode to file
-    if (LLVMWriteBitcodeToFile(mod, "printf.bc") != 0) {
+    if (LLVMWriteBitcodeToFile(module, "codegen.bc") != 0) {
         fprintf(stderr, "error writing bitcode to file, skipping\n");
     }
-    
+
     LLVMDisposeExecutionEngine(engine);
 }
